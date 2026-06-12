575d196ad8ad7b2accfe7407cbe682b5962a456b
rekceq <rekceq@tutamail.com>
2026-06-12 01:34:42 -0300

    Quartz sync: Jun 12, 2026, 1:34 AM

---
+++ quartz/components/scripts/custom.inline.js
@@ -0,0 +1,10 @@
+document.addEventListener("themechange", (e) => {
+    const currentTheme = e.detail.theme; // either "light" or "dark"
+    const imgElement = document.getElementById("kakuji_logo_dark");
+
+    if (currentTheme === "dark") {
+        imgElement.src = "https://github.com/rekceq/becker/blob/v5/content/file_pile/img/kakuji_logo_dark.png?raw=true";
+    } else {
+        imgElement.src = "https://github.com/rekceq/becker/blob/v5/content/file_pile/img/kakuji_logo_light.png?raw=true";
+    }
+});
