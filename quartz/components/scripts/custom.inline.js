022308c94bf49cb751e77364f5936f91af6ec6b4
rekceq <rekceq@tutamail.com>
2026-06-13 02:06:42 -0300

    Quartz sync: Jun 13, 2026, 2:06 AM

--- quartz/components/scripts/custom.inline.js
+++ quartz/components/scripts/custom.inline.js
@@ -1,22 +1,20 @@
-document.addEventListener("themechange", (e) => {
-    const currentTheme = e.detail.theme; // either "light" or "dark"
-    const imgElement = document.getElementById("kakuji_logo_dark.png");
+// document.addEventListener("themechange", (e) => {
+//     const currentTheme = e.detail.theme; // either "light" or "dark"
+//     const imgElement = document.getElementById("kakuji_logo_dark.png");
 
-    if (currentTheme === "dark") {
-        imgElement.src = "https://github.com/rekceq/becker/blob/v5/content/file_pile/img/kakuji_logo_dark.png?raw=true";
-    } else {
-        imgElement.src = "https://github.com/rekceq/becker/blob/v5/content/file_pile/img/kakuji_logo_light.png?raw=true";
+//     if (currentTheme === "dark") {
+//         imgElement.src = "https://github.com/rekceq/becker/blob/v5/content/file_pile/img/kakuji_logo_dark.png?raw=true";
+//     } else {
+//         imgElement.src = "https://github.com/rekceq/becker/blob/v5/content/file_pile/img/kakuji_logo_light.png?raw=true";
+//     }
+// });
+
+document.addEventListener('nav', (e) => {
+  // Check if the current page is a canvas or contains the canvas container
+  if (window.location.pathname.includes('canvas') || document.querySelector('.canvas-container')) {
+    const viewportMeta = document.querySelector('meta[name="viewport"]');
+    if (viewportMeta) {
+      viewportMeta.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no');
     }
+  }
 });
-
-const canvas = document.getElementById('myCanvas');
-
-// Disable scrolling via mouse wheel
-canvas.addEventListener('wheel', function(event) {
-    event.preventDefault();
-}, { passive: false });
-
-// Disable scrolling via touch gestures (Mobile)
-canvas.addEventListener('touchmove', function(event) {
-    event.preventDefault();
-}, { passive: false });
---
+++ quartz/styles/custom.js
@@ -0,0 +1,3 @@
+document.addEventListener("wheel", function(event) {
+    event.preventDefault();
+}, { passive: false });
